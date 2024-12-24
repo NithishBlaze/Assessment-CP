3a282e2995e79c21c1c31baea01af04406cce868
Nithish <blazenithish@gmail.com>
2024-12-24 09:44:23 +0530

    python task modified

--- backEndTasks/python/List_task.py
+++ backEndTasks/python/List_task.py
@@ -1,8 +1,7 @@
 foods = ['burger','veg pizza','momos','chinese','garlic bread','french fries','non-veg pizza']
 print(type(foods))
-count_foods = foods.count
 print('Food available:',foods)
-print('Total food items:',count_foods)
+print('Total food items:',len(foods))
 print('Food at present not available:',foods[2:5])
 foods.append('kabab')
 print('After adding kabab:',foods)
